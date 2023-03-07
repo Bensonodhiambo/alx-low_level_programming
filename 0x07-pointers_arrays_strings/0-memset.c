059a6388a1b78aab87e7e15def4d6d815b51e19e
Bensonodhiambo <benodhiambo.bo@gmail.com>
2023-03-07 10:14:11 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _memcpy - Fill a block of memory with a specific value
+ * @s: Starting address of memory to be filled
+ * @b: The desired value
+ * @n: Number of bits to be changed
+ *
+ * Return: Changed array with new value for n bytes
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
