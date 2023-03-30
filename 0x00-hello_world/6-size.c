74e236e0ad0686fa889aeee3095e2619d1fc111e
Bensonodhiambo <benodhiambo.bo.com>
2023-03-30 16:27:16 -0700

    2-add_node.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,9 +3,17 @@
  * main - A program that prints the size of variuos computer types
  * Return 0 (success)
  */
-int main(void)
+int main() 
 {
-	printf("Size of a char: %zu byte(s)\n|, sizeof(char)):
-	printf("Size of an int: %zu vbyte(s)\n" sizeof(int));
-	printf("
-			 
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+	
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of char: %zu byte\n", sizeof(charType))
+		
+		return 0;
+}			 
