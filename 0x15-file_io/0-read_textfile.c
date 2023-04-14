f9e19d8712f1fc047ab80f1187112b56dc0a3245
Bensonodhiambo <benodhiambo.bo.com>
2023-04-14 16:25:49 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    if (filename == NULL)
+        return 0;
+
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL)
+        return 0;
+
+    char *buffer = (char *) malloc(sizeof(char) * (letters + 1));
+    if (buffer == NULL)
+        return 0;
+
+    ssize_t bytes_read = fread(buffer, sizeof(char), letters, fp);
+    if (bytes_read == -1)
+        return 0;
+
+    buffer[bytes_read] = '\0';
+
+    ssize_t bytes_written = printf("%s", buffer);
+    if (bytes_written == -1 || bytes_written != bytes_read)
+        return 0;
+
+    fclose(fp);
+    free(buffer);
+
+    return bytes_read;
+}
