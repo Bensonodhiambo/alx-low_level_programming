01c9ef7075b3b0890a0d96f182e80c2f579e46d2
Bensonodhiambo <benodhiambo.bo@gmail.com>
2023-06-09 19:45:14 -0700

    updated libmy.a

---
+++ 0x09-static_libraries/10_puts.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ *_puts - returns strings
+ *@s: strings
+ *Return: Prints string
+ */
+void_puts(char *s);
---
+++ 0x09-static_libraries/atoi.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * _atoi - prints string
+ * @s: string
+ * Return: string
+ */
+int _atoi(char *s);
