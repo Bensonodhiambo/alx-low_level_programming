ae8168d3d0918ae2dfbc7736841018e0f8a8d5d5
Bensonodhiambo <benodhiambo.bo@gmail.com>
2023-06-12 09:17:12 -0700

    updated 0-create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,14 +10,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *array;
+	char *str;
 	unsigned int i;
 
-	array = malloc(sizeof(char) * size);
-	if ((size == 0) || (array == NULL))
+	str = malloc(sizeof(char) * size);
+	if ((size == 0) || (str == NULL))
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		array[i] = c;
-	return (array);
+		str[i] = c;
+	return (str);
 }
