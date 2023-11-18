2cafa4d0b47c117e8b4366fd5381512265bd61e8
Bensonodhiambo <benodhiambo.bo@gmail.com>
2023-11-17 17:31:08 -0800

    libdynamic.so

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * _strchr - returns the length of a string
+ * @s: string
+ * @c: character
+ * Return: length
+ */
+char *_strchr(char *s, char c);
---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ *_puts - returns strings
+ *@s: strings
+ *Return: Prints string
+ */
+void _puts(char *s);
