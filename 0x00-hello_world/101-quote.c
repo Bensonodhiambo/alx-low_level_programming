7481473484be8d9e7aeb8cd3b9233f082c738932
Bensonodhiambo <benodhiambo.bo@gmail.com>
2023-12-27 16:38:44 -0800

    101quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-#include<stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include<string.h>
 /**
- * main - A c program that prints a limne to the standard error
+ * main - A c program that prints a line to the standard error
  * Return 1(Success)
  */
 int main(void)
-{
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+{	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2,message, strlen(message));
 	return (1);
 }
